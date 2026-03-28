0f8312e31d8bda8dec14c8fdab01e600b310a3cf
Bryan010205 <vop8227@gmail.com>
2026-03-28 16:01:26 -0700

    setup App.jsx structure for upcoming components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css'
+// Import các component sau khi bạn đã tạo file cho chúng
+// import Navbar from './components/Navbar'
+// import Hero from './components/Hero'
+// import About from './components/About'
+// import CoreValues from './components/CoreValues'
+// import Projects from './components/Projects'
+// import Footer from './components/Footer'
 
 function App() {
-
   return (
-    <>
-      <h1>Bryan;s New Portfolio</h1>
-    </>
+    <div className="portfolio-container">
+      {/* Tạm thời comment lại cho đến khi bạn viết code cho từng file */}
+      {/* <Navbar /> */}
+      {/* <Hero /> */}
+      {/* <About /> */}
+      {/* <CoreValues /> */}
+      {/* <Projects /> */}
+      {/* <Footer /> */}
+      
+      <h1>Bắt đầu code Portfolio tại đây</h1>
+    </div>
   )
 }
 
