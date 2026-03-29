4474b18ec1a8ebc3efa1d266878dc2a71471f70a
Bryan010205 <vop8227@gmail.com>
2026-03-29 08:53:55 -0700

    fix: correct components folder structure and update global background color

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
-import './App.css'
-// Import các component sau khi bạn đã tạo file cho chúng
-// import Navbar from './components/Navbar'
-// import Hero from './components/Hero'
-// import About from './components/About'
-// import CoreValues from './components/CoreValues'
-// import Projects from './components/Projects'
-// import Footer from './components/Footer'
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="portfolio-container">
-      {/* Tạm thời comment lại cho đến khi bạn viết code cho từng file */}
-      {/* <Navbar /> */}
-      {/* <Hero /> */}
-      {/* <About /> */}
-      {/* <CoreValues /> */}
-      {/* <Projects /> */}
-      {/* <Footer /> */}
-      
-      <h1>Bắt đầu code Portfolio tại đây</h1>
+      <Navbar />
+      <Hero />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
